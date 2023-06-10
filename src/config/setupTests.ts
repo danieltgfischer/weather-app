417dfff20cbaa59808bb279a6a127557b2c74fb5
Daniel Fischer <daniel.fischer@ndd.tech>
2023-06-10 19:23:02 -0300

    chore: removed unnecessary mocking

--- src/config/setupTests.ts
+++ src/config/setupTests.ts
@@ -12,8 +12,6 @@ jest.mock('react-native-reanimated', () => {
   return Reanimated;
 });
 
-// Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
-jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
 
 global.window = {};
 

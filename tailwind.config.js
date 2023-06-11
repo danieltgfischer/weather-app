ed672a8d6831a2116295bbbaf2d3fb56620d58dd
Daniel Fischer <daniel.fischer@ndd.tech>
2023-06-11 12:20:34 -0300

    fix: nativewind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./src/components/*.{js,jsx,ts,tsx}", "./src/screens/*.{js,jsx,ts,tsx}"],
+  content: ["./src/**/*.{ts,tsx}"],
   theme: {
     extend: {},
   },
